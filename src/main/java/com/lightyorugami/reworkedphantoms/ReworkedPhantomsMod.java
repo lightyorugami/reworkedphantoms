52224fd144958eb0a09fdfc96e166480ce88758e
lightyorugami <nagengames@gmail.com>
2026-03-17 15:00:44 +0100

    Add files via upload

---
+++ src/main/java/com/lightyorugami/reworkedphantoms/ReworkedPhantomsMod.java
@@ -0,0 +1,41 @@
+package com.lightyorugami.reworkedphantoms;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.GameRules;
+import net.neoforged.neoforge.event.server.ServerStartedEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.common.EventBusSubscriber;
+import org.slf4j.Logger;
+
+@Mod(ReworkedPhantomsMod.MODID)
+public class ReworkedPhantomsMod {
+    public static final String MODID = "reworkedphantoms";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public static GameRules.Key<GameRules.BooleanValue> RULE_DO_PHANTOM_SPAWNING;
+
+    public ReworkedPhantomsMod() {
+        LOGGER.info("Reworked Phantoms Mod loaded!");
+        RULE_DO_PHANTOM_SPAWNING = GameRules.register(
+            "doPhantomSpawning",
+            GameRules.Category.SPAWNING,
+            GameRules.BooleanValue.create(true)
+        );
+    }
+
+    @EventBusSubscriber(modid = MODID)
+    public static class ServerEvents {
+        @SubscribeEvent
+        public static void onServerStarted(ServerStartedEvent event) {
+            MinecraftServer server = event.getServer();
+            for (ServerLevel level : server.getAllLevels()) {
+                level.getGameRules().getRule(GameRules.RULE_DOINSOMNIA).set(false, server);
+                LOGGER.info("doInsomnia has been set to false in level: " + level.dimension().location());
+            }
+            LOGGER.info("Custom gamerule 'doPhantomSpawning' registered (default: true)");
+        }
+    }
+}
